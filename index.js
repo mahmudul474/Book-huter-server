12983de531f3de8d5505fb67ad36f0ff55b3885f
mahmudul474 <mdmahmudulla474@gmail.com>
2023-11-08 14:37:31 +0600

    delet product done

--- index.js
+++ index.js
@@ -1,68 +1,130 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port =process.env.PORT ||5000
-const  cors = require('cors')
-require('dotenv').config()
-
-
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+require("dotenv").config();
 
 //middleware
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
+///database  connect
 
-///database  connect 
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
+const uri = `mongodb+srv://Assainment-5:HyAXDpnLTARhmibL@474.79d3jxt.mongodb.net/?retryWrites=true&w=majority`; // Your MongoDB server URL
+const client = new MongoClient(uri);
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@474.79d3jxt.mongodb.net/?retryWrites=true&w=majority";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
+async function connectToMongoDB() {
   try {
-    
-const bookCollection =client.db("book-huter").collection("books");
+    await client.connect();
+    const bookCollection = client.db("book-huter").collection("books");
 
+    app.post("/book", async (req, res) => {
+      try {
+        const book = req.body;
+        const result = await bookCollection.insertOne(book);
+        res.status(200).json({ data: result, msg: "Book added successfully" });
+      } catch (error) {
+        res.status(500).json({ msg: "A Eroor founde" });
+        console.log(error);
+      }
+    });
 
-app.post("/book", async (req, res) => {
-    try {
-      const book = req.body;
-      const result = await bookCollection.insertOne(book);
-      res.status(200).json({ data: result, msg: 'Book added successfully' });
-    } catch (error) {
-      res.status(500).json({ msg: 'A Eroor founde' });
-    }
-  });
-  
+    app.get("/books", async (req, res) => {
+      try {
+        const books = await bookCollection
+          .find({})
+          .sort({ publicationDate: -1 })
+          .limit(10)
+          .toArray();
+        res.status(200).json(books);
+      } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+      }
+    });
 
+    app.get("/book/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const book = await bookCollection.findOne(query);
+        res.status(200).json(book);
+      } catch (error) {
+        res.status(500).json({ error: "server no response " });
+      }
+    });
 
+    app.put("/book/review/:id", async (req, res) => {
+      try {
+        const newReview = req.body;
+        const bookId = req.params.id;
+        const query = { _id: new ObjectId(bookId) };
+        const result = await bookCollection.updateOne(query, {
+          $push: { reviews: newReview },
+        });
 
-} finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+        console.log(result, "check  result ");
+        if (result.modifiedCount === 1) {
+          res.status(200).json({ message: "Review added successfully." });
+        } else {
+          res
+            .status(404)
+            .json({ message: "Book not found or review not added." });
+        }
+      } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ message: "Internal server error." });
+      }
+    });
+
+    app.delete("/book/delete/:id", function (req, res) {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = bookCollection.deleteOne(query);
+      if (result.deletedCount === 1) {
+        res.status(200).json({ message: "Book deleted successfully." });
+      } else {
+        res
+          .status(404)
+          .json({ message: "Book not found or review not added." });
+      }
+    });
+
+    app.put("/book/edit/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updatedBook = req.body.book;
+      const { authorName, title, genre, publicationDate } = updatedBook;
+
+      try {
+        const result = await bookCollection.updateOne(query, {
+          $set: { authorName, title, genre, publicationDate },
+        });
+
+        if (result.modifiedCount === 1) {
+          res.status(200).json({ message: "Book updated successfully." });
+        } else {
+          res.status(404).json({ message: "Book not found or update failed." });
+        }
+      } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ message: "Internal server error." });
+      }
+    });
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
   }
 }
-run().catch(console.dir);
 
-
-
-
+// database connection
+connectToMongoDB();
 
 app.get("/", (req, res) => {
-    res.send("Welcome assignment 5  frontend!");
+  res.send("Welcome assignment 5  frontend!");
 });
 
-
-
 app.listen(port, () => {
-    console.log(`Server started on port`, port);
+  console.log(`Server started on port`, port);
 });
